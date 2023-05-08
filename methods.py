8b1581fb2a10caabd6695b619c1da9c1d25e19fc
PeloConejo25 <22.00448-3@maua.br>
2023-05-08 09:06:40 -0300

    aula_09_ci_cd_Coelho
    Métodos adicionados:
    - Adição
    - Subtração
    - Multiplicação
    - Divisão

--- methods.py
+++ methods.py
@@ -5,3 +5,19 @@ def area_of_rectangle(width, height):
 def perimeter_of_rectangle(width, height):
     perimeter = 2 * (width + height)
     return perimeter
+
+def sum_of_numbers(number1, number2):
+    sum = number1 + number2
+    return sum
+
+def sub_of_numbers(number1, number2):
+    sub = number1 - number2
+    return sub
+
+def multiply_numbers(number1, number2):
+    mult = number1 * number2
+    return mult
+
+def divide_numbers(number1, number2):
+    divis = number1 / number2
+    return divis
--- test_methods.py
+++ test_methods.py
@@ -21,3 +21,48 @@ def test_perimeter():
     
     # then the perimeter should be 14
     assert output == 14
+
+def test_sum():
+    # given two numbers, 2 and 5
+    number1 = 2
+    number2 = 5
+
+    # when we calculate the sum
+    output = methods.sum_of_numbers(number1, number2)
+
+    # then the total should be 7
+    assert output == 7
+
+def test_sub():
+    # given two numbers, 9 and 6
+    number1 = 9
+    number2 = 6
+
+    # when we calculate the subtraction
+    output = methods.sub_of_numbers(number1, number2)
+
+    # then the total should be 3
+    assert output == 3
+
+def test_multiply():
+    # given two numbers, 32 and 2
+    number1 = 32
+    number2 = 2
+
+    # when we calculate the multiplication
+    output = methods.multiply_numbers(number1, number2)
+
+    # then the total should be 64
+    assert output == 64
+
+def test_divide():
+    # given two numbers, 125 and 5
+    number1 = 125
+    number2 = 5
+
+    # when we calculate the division of the first number by the second number
+    output = methods.divide_numbers(number1, number2)
+
+    # then the total should be 25
+    assert output == 25
+
